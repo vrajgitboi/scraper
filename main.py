69647a9f7910a96b576e0c5baec5cfe11f01aa08
vrajgitboi <pankajpatel102005@gmail.com>
2025-07-21 15:28:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
     os.makedirs(base_dir, exist_ok=True)
 
     try:
-        driver.save_screenshot("data/screenshot_start.png")
         scraper = MultiPropertyZillowScraper(headless=headless)
         
     except Exception as e:
